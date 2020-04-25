b7c2bd1e9798a2797861c861287769c85a37f8fa
Matthew Brookson <matthewbrookson@gmail.com>
2020-04-25 13:59:10 +0100

    docs(example): Improve example site header responsiveness

--- example/src/Header.tsx
+++ example/src/Header.tsx
@@ -7,10 +7,10 @@ export const Header = () => {
   return (
     <div className="bg-blue-800 text-white py-24 text-center md:text-right">
       <div className="container">
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="flex-1 md:pr-16">
-            <h1 className="mb-4 text-5xl font-bold">React Simple Modal</h1>
-            <div className="flex flex-row items-center justify-center md:justify-end pb-8">
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="lg:pr-16">
+            <h1 className="mb-4 text-5xl font-bold">React Simple Hook Modal</h1>
+            <div className="flex flex-row items-center justify-center lg:justify-end pb-8">
               <FaGithub className="inline" />
               <a
                 className="ml-2 hover:text-blue-300 transition transition-colors duration-200"
